485e1017f1412fc5472d87164fd2cf38a5169054
stevejayne <stevejayne1999@gmail.com>
2023-04-02 18:17:44 -0400

    added hints for parameters

--- simulation_parameters.h
+++ simulation_parameters.h
@@ -1,11 +1,11 @@
 #include "simulation_data_structures.h"
 
 // Streaming parameters
-#define NUM_PRODUCERS 1
-#define NUM_CONSUMERS 5
+#define NUM_PRODUCERS 1 // Options: 1-30
+#define NUM_CONSUMERS 5 // Options: 1-30
 
-#define CODEC_TYPE h264
+#define CODEC_TYPE h264 // Options: h264 or h265
 
 
 // Network Parameterss
-#define NETWORK_BANDWITDH 100000
+#define NETWORK_BANDWITDH 100000 // Options: 1000-1000000
